423239ac0034401c892496bf3f1811fde72ac34a
Valentyn <valentyncherkasov24@gmail.com>
2021-03-04 18:12:36 +0200

    refactor: provide 2 ways of writing (by one operation and line by line)

--- csv_tasks.py
+++ csv_tasks.py
@@ -1,13 +1,27 @@
 import csv
 
-with open("friends.csv", "w", newline="") as csvfile:
-    writer = csv.writer(
-        csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
-    )
-    writer.writerow(["Alex"] + ["02.05.2001"] + ["Software Developer"])
-    writer.writerow(["Dima"] + ["10.11.2000"] + ["UX/UI"])
+friends_string = [
+    ["Alex, 02.05.2001, Software Developer"],
+    ["Dima, 10.11.2000, UX/UI"],
+    ["Valentyn, 03.02.2001, Python Dev"],
+]
 
-with open("friends.csv", newline="") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
-    for row in spamreader:
-        print(", ".join(row))
+
+def write_by_one_operation(lst):
+    with open("friends.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        writer.writerows(lst)
+
+
+def write_line_by_line(lst):
+    with open("friends.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        for row in lst:
+            writer.writerow(row)
+
+
+def read_csv():
+    with open("friends.csv", "r", newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=" ", quotechar="|")
+        for row in reader:
+            print(", ".join(row))
